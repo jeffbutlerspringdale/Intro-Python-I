b53200de0ea24242ccded7da8d85e3ee3434f00f
jeff.butler.springdale@gmail.com <jeff.butler.springdale@gmail.com>
2019-12-04 08:47:29 -0600

    mostly done

--- src/03_modules.py
+++ src/03_modules.py
@@ -6,26 +6,28 @@ level operating system functionality.
 """
 
 import sys
+import os
+import platform
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
-
+print("system arg: ", sys.argv)
 # Print out the OS platform you're using:
 # YOUR CODE HERE
-
+print("OS.name: ", os.name)
+print("Platform.system: ", platform.system())
 # Print out the version of Python you're using:
 # YOUR CODE HERE
-
-
-import os
+print("Current python version: ", sys.version)
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
-
+print("current PID: ", os.getpid())
 # Print the current working directory (cwd):
 # YOUR CODE HERE
-
+print("Current working directory: ", os.getcwd())
 # Print out your machine's login name
 # YOUR CODE HERE
+print("machines login name: ", os.getlogin())
--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -11,24 +11,32 @@ for more info regarding list comprehensions.
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
 
 y = []
-
-print (y)
+for x in range(6):
+    y.append(x + 1)
+print("first y", y)
 
 # Write a list comprehension to produce the cubes of the numbers 0-9:
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
-
 y = []
 
-print(y)
+y.append(0)
+for x in range(9):
+    y.append((x + 1) ** 3)
+print("second y", y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
-
-a = ["foo", "bar", "baz"]
-
 y = []
 
-print(y)
+a = ["foo", "bar", "baz"]
+# y = [a[0].upper(), a[1].upper(), a[2].upper()]
+# for x in range(len(a)):
+#     y.append((a[0]).upper())
+# for x in a:
+#     y.append(str.upper(a[0]))
+
+y = [w.upper() for w in a]
+print("third y", y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
@@ -36,6 +44,9 @@ print(y)
 x = input("Enter comma-separated numbers: ").split(',')
 
 # What do you need between the square brackets to make it work?
-y = []
+# y = []
+import numpy as np
 
-print(y)
+x = np.array(x, dtype=np.float64)
+x = [num for num in x if num % 2 == 0]
+print("fourth y", x)
